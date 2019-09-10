d58114695b08181c7a211da43f67bd011f84d94a
marta1511 <50339660+marta1511@users.noreply.github.com>
2019-09-10 21:53:58 +0200

    Update ObradaKlijent

--- Projekt/src/main/java/edunova/controller/ObradaKlijent.java
+++ Projekt/src/main/java/edunova/controller/ObradaKlijent.java
@@ -15,18 +15,47 @@ import java.util.List;
  */
 public class ObradaKlijent extends Obrada <Klijent> {
     
-    public List <Klijent> getKlijent (){
-    return session.createQuery("from Klijent").list();
+   
+
+    @Override
+    protected void KontrolaSpremi(Klijent entitet) throws EdunovaException {
+        kontrolaIme(entitet);
+        kontrolaPrezime(entitet);
+        kontrolaTelefon(entitet);
+        kontrolaBrojVozacke(entitet);
     }
 
     @Override
-    protected void KontrolaSpremi() throws EdunovaException {
-       
-    }
-
-    @Override
-    protected void KontrolaBrisi() throws EdunovaException {
+    protected void KontrolaBrisi(Klijent entitet) throws EdunovaException {
         
     }
+
+    @Override
+    public List<Klijent> getEntiteti() {
+         return session.createQuery("from Klijent").list();
+    }
     
+    private void kontrolaIme (Klijent entitet) throws EdunovaException{
+    if(entitet.getIme() == null || entitet.getIme().trim().length()==0){
+    throw new EdunovaException("Obavezno unijeti ime klijenta");}
+    }
+     private void kontrolaPrezime (Klijent entitet) throws EdunovaException{
+    if(entitet.getPrezime()== null || entitet.getPrezime().trim().length()==0){
+    throw new EdunovaException("Obavezno unijeti prezime klijenta");}
+    }
+     
+      private void kontrolaBrojVozacke(Klijent entitet) throws EdunovaException {
+        if (entitet.getBrojVozacke().trim().length() <= 0 || entitet.getBrojVozacke().trim().length()> 8) {
+            throw new EdunovaException("Broj vozačke mora biti 8 znakova");
+                 
+        }else if(entitet.getBrojVozacke()== null){
+            throw new EdunovaException("Broj vozačke mora biti unesen");
+            
+        }
+      }
+    
+    private void kontrolaTelefon(Klijent entitet) throws EdunovaException{
+    if (entitet.getTelefon()== null || entitet.getTelefon().trim().length()==0){
+    throw new EdunovaException("Broj telefona klijenta mora biti unesen");}
+    }
 }
