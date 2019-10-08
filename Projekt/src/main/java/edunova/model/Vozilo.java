9690b13839713099ed6117bd7d17e74a5ccde39e
marta1511 <50339660+marta1511@users.noreply.github.com>
2019-10-08 19:45:22 +0200

    brisanje iz modela i dodavanje u vozilo

--- Projekt/src/main/java/edunova/model/Vozilo.java
+++ Projekt/src/main/java/edunova/model/Vozilo.java
@@ -20,27 +20,32 @@ public class Vozilo extends Entitet implements Serializable{
     
     private String registracijaskaOznaka;
     private Date datumRegistracije;
+    private String naziv;
+    private String marka;
     
-    @ManyToOne
-    private Model model;
+    private Integer brojSjedala;
+    private String vrstaMotora;
+    private Boolean automatik;
+    private Date godinaProizvodnje;
+    private BigDecimal cijenaPoDanu;
 
     public Vozilo() {
         super();
     }
+    
+    
 
-    public Vozilo(String registracijaskaOznaka, Date datumRegistracije, Model model, Integer sifra) {
+    public Vozilo(String registracijaskaOznaka, Date datumRegistracije, String naziv, String marka, Integer brojSjedala, String vrstaMotora, Boolean automatik, Date godinaProizvodnje, BigDecimal cijenaPoDanu, Integer sifra) {
         super(sifra);
         this.registracijaskaOznaka = registracijaskaOznaka;
         this.datumRegistracije = datumRegistracije;
-        this.model = model;
-    }
-
-    public Model getModel() {
-        return model;
-    }
-
-    public void setModel(Model model) {
-        this.model = model;
+        this.naziv = naziv;
+        this.marka = marka;
+        this.brojSjedala = brojSjedala;
+        this.vrstaMotora = vrstaMotora;
+        this.automatik = automatik;
+        this.godinaProizvodnje = godinaProizvodnje;
+        this.cijenaPoDanu = cijenaPoDanu;
     }
     
     
@@ -61,6 +66,66 @@ public class Vozilo extends Entitet implements Serializable{
     public void setDatumRegistracije(Date datumRegistracije) {
         this.datumRegistracije = datumRegistracije;
     }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public String getMarka() {
+        return marka;
+    }
+
+    public void setMarka(String marka) {
+        this.marka = marka;
+    }
+
+    public Integer getBrojSjedala() {
+        return brojSjedala;
+    }
+
+    public void setBrojSjedala(Integer brojSjedala) {
+        this.brojSjedala = brojSjedala;
+    }
+
+    public String getVrstaMotora() {
+        return vrstaMotora;
+    }
+
+    public void setVrstaMotora(String vrstaMotora) {
+        this.vrstaMotora = vrstaMotora;
+    }
+
+    public Boolean getAutomatik() {
+        return automatik;
+    }
+
+    public void setAutomatik(Boolean automatik) {
+        this.automatik = automatik;
+    }
+
+    public Date getGodinaProizvodnje() {
+        return godinaProizvodnje;
+    }
+
+    public void setGodinaProizvodnje(Date godinaProizvodnje) {
+        this.godinaProizvodnje = godinaProizvodnje;
+    }
+
+    public BigDecimal getCijenaPoDanu() {
+        return cijenaPoDanu;
+    }
+
+    public void setCijenaPoDanu(BigDecimal cijenaPoDanu) {
+        this.cijenaPoDanu = cijenaPoDanu;
+    }
     
     
+     @Override
+    public String toString() {
+        return naziv;
+}
 }
